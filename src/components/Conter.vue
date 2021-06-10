dcd4075308f5f34e337252ae3d8ed5e0b4d911ab
brunorufino <69933497+brunorufino@users.noreply.github.com>
2021-06-10 10:31:21 -0300

    Add files via upload

---
+++ src/components/Conter.vue
@@ -0,0 +1,30 @@
+<template>
+   <div class="counter">
+    <h1> Counter {{counter}} </h1>
+    <button @click="decrement"> Decrement </button>
+    <button @click="increment"> Increment </button>
+ </div> 
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+export default {
+    name: 'Counter',
+     computed :{
+    ... mapGetters([
+                    'counter'
+                    ]
+                )
+  },
+   methods: {
+       ...mapActions([
+           'decrement',
+           'increment'
+       ])
+   },
+}
+</script>
+
+<style>
+
+</style>
